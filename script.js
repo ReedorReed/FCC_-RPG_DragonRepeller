da9fcd9017e3315dc603cdf0c51a622dc79da215
Christian Reed <chreed87@gmail.com>
2024-10-07 14:45:49 +0200

    added some html and styles plus some variables in js

---
+++ script.js
@@ -0,0 +1,11 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let inventory = ['stick'];
+let monsterHealth;
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
